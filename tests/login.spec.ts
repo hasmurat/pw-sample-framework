db28fa21654b166946c3f671aa28158fcabe2248
hasmurat <muratkangal0101@gmail.com>
2026-05-28 23:32:43 +0300

    added tags

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,9 +2,17 @@ import { test } from "../testOptions";
 
 test.use({ storageState: { cookies: [], origins: [] } });
 
-test("As a user, I should be able to login and logout successfully", async ({ pageManager }) => {
-  await pageManager.getLoginPage().navigateTo('login');
-  await pageManager.getLoginPage().login(process.env.EMAIL_ADDRESS!, process.env.PASSWORD!);
-  await pageManager.getLoginPage().logout();
-});
+test(
+  "As a user, I should be able to login and logout successfully",
+  { tag: ["@smoke", "@regression"] },
+  async ({ pageManager }) => {
 
+    await pageManager.getLoginPage().navigateTo("login");
+    await pageManager
+      .getLoginPage()
+      .login(process.env.EMAIL_ADDRESS!, process.env.PASSWORD!);
+      
+    await pageManager.getLoginPage().logout();
+  },
+
+);
--- tests/order-flow.spec.ts
+++ tests/order-flow.spec.ts
@@ -4,59 +4,65 @@ import { getTestData } from "../utils/dataLoader";
 
 const checkOutData = getTestData("checkOutData");
 
-test("As a user, I should be able to create an order, complete the checkout process, and track the order", async ({ pageManager }) => {
-  // Navigate to the shop page and add a product to the cart
-  await pageManager.getProductsPage().navigateToPage("Shop");
-  
-  // Add Brazilian Santos to the cart and get its price
-  await pageManager.getProductsPage().addProductToCart("Brazilian Santos");
-  const productPrice = await pageManager
-    .getProductsPage()
-    .getProductPrice("Brazilian Santos");
-  
-  // Navigate to Cart page
-  await pageManager.getCartPage().clickCartButton();
+test(
+  "As a user, I should be able to create an order, complete the checkout process, and track the order",
+  { tag: "@regression" },
+  async ({ pageManager }) => {
 
-  // Assert the selected product is in the Cart and its price is correct
-  await pageManager.getCartPage().assertProductInCart("Brazilian Santos");
-  await expect(pageManager.getCartPage().getSubTotal()).resolves.toBe(
-    productPrice,
-  );
+    // Navigate to the shop page and add a product to the cart
+    await pageManager.getProductsPage().navigateToPage("Shop");
 
-  // Navigate to Checkout page
-  await pageManager.getCartPage().clickProceedToCheckout();
+    // Add Brazilian Santos to the cart and get its price
+    await pageManager.getProductsPage().addProductToCart("Brazilian Santos");
+    const productPrice = await pageManager
+      .getProductsPage()
+      .getProductPrice("Brazilian Santos");
 
-  await pageManager.getCheckoutPage().verifyEmailAddress();
+    // Navigate to Cart page
+    await pageManager.getCartPage().clickCartButton();
 
-  // Fill in the checkout form and submit the order
-  await pageManager
-    .getCheckoutPage()
-    .fillShippingAddress(
-      checkOutData.shipping_address.address,
-      checkOutData.shipping_address.city,
-      checkOutData.shipping_address.zipCode,
-      checkOutData.shipping_address.country,
+    // Assert the selected product is in the Cart and its price is correct
+    await pageManager.getCartPage().assertProductInCart("Brazilian Santos");
+    await expect(pageManager.getCartPage().getSubTotal()).resolves.toBe(
+      productPrice,
     );
 
-  await pageManager
-    .getCheckoutPage()
-    .fillPaymentDetails(
-      checkOutData.payment_information.nameOnCard,
-      checkOutData.payment_information.cardNumber,
-      checkOutData.payment_information.expiry,
-      checkOutData.payment_information.cvc,
-    );
+    // Navigate to Checkout page
+    await pageManager.getCartPage().clickProceedToCheckout();
 
-  // Click Place Order button and assert confirmation
-  await pageManager.getCheckoutPage().clickPlaceOrder();
+    await pageManager.getCheckoutPage().verifyEmailAddress();
 
-  // Get Order ID
-  const orderID = await pageManager.getCheckoutPage().getOrderID();
-  
-  // Navigate to Contact page
-  await pageManager.getCheckoutPage().navigateToPage("Contact");
+    // Fill in the checkout form and submit the order
+    await pageManager
+      .getCheckoutPage()
+      .fillShippingAddress(
+        checkOutData.shipping_address.address,
+        checkOutData.shipping_address.city,
+        checkOutData.shipping_address.zipCode,
+        checkOutData.shipping_address.country,
+      );
 
-  // Fill in the order tracking form and submit
-  await pageManager.getContactPage().trackOrder(orderID, process.env.EMAIL_ADDRESS || "");
+    await pageManager
+      .getCheckoutPage()
+      .fillPaymentDetails(
+        checkOutData.payment_information.nameOnCard,
+        checkOutData.payment_information.cardNumber,
+        checkOutData.payment_information.expiry,
+        checkOutData.payment_information.cvc,
+      );
 
-});
+    // Click Place Order button and assert confirmation
+    await pageManager.getCheckoutPage().clickPlaceOrder();
+
+    // Get Order ID
+    const orderID = await pageManager.getCheckoutPage().getOrderID();
+
+    // Navigate to Contact page
+    await pageManager.getCheckoutPage().navigateToPage("Contact");
+
+    // Fill in the order tracking form and submit
+    await pageManager
+      .getContactPage()
+      .trackOrder(orderID, process.env.EMAIL_ADDRESS || "");
+  },
+);
