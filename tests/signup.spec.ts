b16cab0fbdc2574593af7f2bdae8a58470f83b4a
hasmurat <muratkangal0101@gmail.com>
2026-05-28 23:17:50 +0300

    added explanation for signup scenario

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -4,12 +4,12 @@ import * as emailUtils from "../utils/emailUtils";
 
 test.use({ storageState: { cookies: [], origins: [] } });
 const signupData = getTestData("signupData");
-const testSignUp = process.env.SIGN_UP_FLOW
+const testSignUp = process.env.SIGN_UP_FLOW // I used trial account of mailslurp for this test, and I don't want to run it every time. So I added an environment variable to control whether to run this test or not.
 
 test("As a user, I should be able to sign up successfully and receive a confirmation email", async ({pageManager}) => {
 
   test.skip(testSignUp !== 'true', 'Skipping sign up test');
-  
+
   await pageManager.getSignupPage().navigateTo("signup");
   await pageManager.getSignupPage().fillSignupForm(
     signupData.firstName,
