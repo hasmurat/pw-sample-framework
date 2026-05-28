2357c86af06a42ce5faaffd17a1f88b2586bbab1
hasmurat <muratkangal0101@gmail.com>
2026-05-28 20:40:12 +0300

    added page description to setup

--- setup/auth.setup.ts
+++ setup/auth.setup.ts
@@ -1,6 +1,12 @@
 import { test as setup } from "../testOptions";
 import path from "path";
 
+/**
+ * Description: This setup file is responsible for performing authentication before running the tests. 
+ * It uses the test function from testOptions to define a setup step that logs in to the application and saves the authenticated state to a file. 
+ * This allows the tests to run with an authenticated session without having to log in before each test.
+ */
+
 const authFile = path.join(__dirname, "../playwright/.auth/user.json");
 
 setup("authentication", async ({ pageManager }) => {
